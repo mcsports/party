873305473ee5445c70a08bd465b6399fb8862288
jannik <jannik.developer02@gmail.com>
2025-05-24 02:27:36 +0200

    add levels to role

--- shared/src/main/kotlin/club/mcsports/droplet/party/shared/member/PartyRole.kt
+++ shared/src/main/kotlin/club/mcsports/droplet/party/shared/member/PartyRole.kt
@@ -1,9 +1,11 @@
 package club.mcsports.droplet.party.shared.member
 
-enum class PartyRole {
+enum class PartyRole(
+    val strength: Int
+) {
 
-    OWNER,
-    MOD,
-    MEMBER;
+    OWNER(2),
+    MOD(1),
+    MEMBER(0);
 
 }
