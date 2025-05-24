33a5923f2bfa42d7018d412c593274bab1636ae8
jannik <jannik.developer02@gmail.com>
2025-05-24 14:36:17 +0200

    add some api functions

--- api/src/main/kotlin/club/mcsports/droplet/party/api/PartyApi.kt
+++ api/src/main/kotlin/club/mcsports/droplet/party/api/PartyApi.kt
@@ -4,6 +4,7 @@ import club.mcsports.droplet.party.api.impl.coroutine.PartyApiCoroutineImpl
 import club.mcsports.droplet.party.api.impl.future.PartyApiFutureImpl
 import club.mcsports.droplet.party.shared.Party
 import club.mcsports.droplet.party.shared.PartySettings
+import io.grpc.Status
 import java.util.*
 import java.util.concurrent.CompletableFuture
 
@@ -21,19 +22,30 @@ interface PartyApi {
             settings: PartySettings
         ): Party
 
+        fun invitePartyMember(memberId: UUID, executor: UUID)
+        fun joinPartyMember(memberId: UUID, partyId: UUID)
+        fun kickPartyMember(memberId: UUID, partyId: UUID, executor: UUID)
+        fun promotePartyMember(memberId: UUID, partyId: UUID, executor: UUID)
+        fun demotePartyMember(memberId: UUID, partyId: UUID, executor: UUID)
     }
 
     interface Future {
 
-        fun getParty(id: UUID): CompletableFuture<Party>
+        fun getParty(partyId: UUID): CompletableFuture<Party>
 
-        fun deleteParty(id: UUID): CompletableFuture<Void>
+        fun deleteParty(partyId: UUID, executor: UUID)
 
         fun createParty(
             creator: UUID,
             initialInvites: MutableSet<UUID>,
             settings: PartySettings
         ): CompletableFuture<Party>
+
+        fun invitePartyMember(memberId: UUID, executor: UUID)
+        fun joinPartyMember(memberId: UUID, partyId: UUID)
+        fun kickPartyMember(memberId: UUID, partyId: UUID, executor: UUID)
+        fun promotePartyMember(memberId: UUID, partyId: UUID, executor: UUID)
+        fun demotePartyMember(memberId: UUID, partyId: UUID, executor: UUID)
 
     }
 
