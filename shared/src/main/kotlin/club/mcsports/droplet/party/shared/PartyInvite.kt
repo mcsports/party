79c359ef42408a5ff4f04ca00768ea1f56a02d30
jannik <jannik.developer02@gmail.com>
2025-05-26 00:50:14 +0200

    delete unnecessary files because we now use protobuf

--- shared/src/main/kotlin/club/mcsports/droplet/party/shared/PartyInvite.kt
+++
@@ -1,8 +0,0 @@
-package club.mcsports.droplet.party.shared
-
-import java.util.UUID
-
-data class PartyInvite(
-    val creator: UUID,
-    val timeCreated: Long = System.currentTimeMillis()
-)
--- shared/src/main/kotlin/club/mcsports/droplet/party/shared/player/PartyInformationHolder.kt
+++
@@ -1,9 +0,0 @@
-package club.mcsports.droplet.party.shared.player
-
-import java.util.UUID
-
-/**
- * Stores data that you might've to get very fast without iterating through lists
- * At the moment it's only the current party of the player, if he is in one at all.
- */
-data class PartyInformationHolder(var partyId: UUID?)
