665b9d612dcc118c04502e66bf88cb20901c0f52
jannik <jannik.developer02@gmail.com>
2025-05-26 00:48:53 +0200

    delete unnecessary files because we now use protobuf

--- shared/src/main/kotlin/club/mcsports/droplet/party/shared/PartySettings.kt
+++
@@ -1,6 +0,0 @@
-package club.mcsports.droplet.party.shared
-
-data class PartySettings(
-    var allowInvites: Boolean = true,
-    var isPrivate: Boolean = true,
-)
--- shared/src/main/kotlin/club/mcsports/droplet/party/shared/member/PartyMember.kt
+++
@@ -1,6 +0,0 @@
-package club.mcsports.droplet.party.shared.member
-
-data class PartyMember(
-    var role: PartyRole,
-    val timeJoined: Long = System.currentTimeMillis()
-)
