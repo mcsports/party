a99158af8e099b057d645bb1cbd3336817195ac0
jannik <jannik.developer02@gmail.com>
2025-05-30 13:50:08 +0200

    update grpc backend to names instead of uuids

--- runtime/src/main/kotlin/club/mcsports/droplet/party/service/PartyDataService.kt
+++ runtime/src/main/kotlin/club/mcsports/droplet/party/service/PartyDataService.kt
@@ -1,7 +1,7 @@
 package club.mcsports.droplet.party.service
 
 import club.mcsports.droplet.party.PartyManager
-import club.mcsports.droplet.party.extension.asUuid
+import club.mcsports.droplet.party.extension.fetchPlayer
 import com.mcsports.party.v1.PartyDataGrpcKt
 import com.mcsports.party.v1.PartyRequest
 import com.mcsports.party.v1.PartyResponse
@@ -12,17 +12,18 @@ class PartyDataService(private val partyManager: PartyManager) : PartyDataGrpcKt
 
     override suspend fun getParty(request: PartyRequest): PartyResponse {
         return partyResponse {
-            val informationHolder = partyManager.informationHolder(request.memberId.asUuid())
+            val memberName = request.memberId.fetchPlayer().getName()
+            val informationHolder = partyManager.informationHolder(memberName)
 
             val partyId = informationHolder.partyId
-                ?: throw Status.NOT_FOUND.withDescription("User ${request.memberId} isn't part of any party")
+                ?: throw Status.NOT_FOUND.withDescription("User $memberName isn't part of any party")
                     .asRuntimeException()
 
             val party = partyManager.parties[partyId]
                 ?: run {
                     informationHolder.partyId = null
 
-                    throw Status.UNAVAILABLE.withDescription("Party ${request.memberId} not found")
+                    throw Status.UNAVAILABLE.withDescription("Party $memberName not found")
                         .asRuntimeException()
                 }
 
