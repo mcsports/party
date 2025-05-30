78668b8dcf1dff46a3e8a0be54da26a0a18e3342
jannik <jannik.developer02@gmail.com>
2025-05-30 14:50:36 +0200

    didnt know simplecloud has that stuff built-in lol

--- runtime/src/main/kotlin/club/mcsports/droplet/party/extension/MiniMessageExtension.kt
+++
@@ -1,5 +0,0 @@
-package club.mcsports.droplet.party.extension
-
-import net.kyori.adventure.text.minimessage.MiniMessage
-
-fun miniMessage(message: String) = MiniMessage.miniMessage().deserialize(message)
