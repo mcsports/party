364b139f871beb0e40aca67fd199e524c6ea8d8a
Jannik <jannik.developer02@gmail.com>
2025-05-31 16:05:37 +0200

    restructure code leck meine eier

--- runtime/src/main/kotlin/club/mcsports/droplet/party/service/PartyDataService.kt
+++ runtime/src/main/kotlin/club/mcsports/droplet/party/service/PartyDataService.kt
@@ -2,31 +2,35 @@ package club.mcsports.droplet.party.service
 
 import club.mcsports.droplet.party.PartyManager
 import club.mcsports.droplet.party.extension.fetchPlayer
+import club.mcsports.droplet.party.extension.log
 import com.mcsports.party.v1.PartyDataGrpcKt
 import com.mcsports.party.v1.PartyRequest
 import com.mcsports.party.v1.PartyResponse
 import com.mcsports.party.v1.partyResponse
 import io.grpc.Status
+import org.apache.logging.log4j.LogManager
 
 class PartyDataService(private val partyManager: PartyManager) : PartyDataGrpcKt.PartyDataCoroutineImplBase() {
 
+    private val logger = LogManager.getLogger(PartyDataService::class.java)
     override suspend fun getParty(request: PartyRequest): PartyResponse {
-        return partyResponse {
-            val memberName = request.memberId.fetchPlayer().getName()
-            val informationHolder = partyManager.informationHolder(memberName)
 
-            val partyId = informationHolder.partyId
-                ?: throw Status.NOT_FOUND.withDescription("User $memberName isn't part of any party")
+        val memberName = request.memberId.fetchPlayer().getName()
+        val informationHolder = partyManager.informationHolder(memberName)
+
+        val partyId = informationHolder.partyId
+            ?: throw Status.NOT_FOUND.withDescription("User $memberName isn't part of any party")
+                .log(logger).asRuntimeException()
+
+        val party = partyManager.parties[partyId]
+            ?: run {
+                informationHolder.partyId = null
+
+                throw Status.UNAVAILABLE.withDescription("Party $memberName not found").log(logger)
                     .asRuntimeException()
+            }
 
-            val party = partyManager.parties[partyId]
-                ?: run {
-                    informationHolder.partyId = null
-
-                    throw Status.UNAVAILABLE.withDescription("Party $memberName not found")
-                        .asRuntimeException()
-                }
-
+        return partyResponse {
             this.party = party
         }
     }
