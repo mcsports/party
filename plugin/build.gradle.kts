e5c751c016c2046328c6fc2190a6a524e514ca07
Jannik <jannik.developer02@gmail.com>
2025-05-31 16:04:17 +0200

    relocate with correct paths

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -11,9 +11,9 @@ dependencies {
 tasks.shadowJar {
     exclude("io.netty")
     exclude("app.simplecloud")
-    relocate("io.grpc", "club.mcsports.droplet.queue.relocate.io.grpc")
-    relocate("com.google.protobuf", "club.mcsports.droplet.queue.relocate.google.protobuf")
-    relocate("com.google.common", "club.mcsports.droplet.queue.relocate.google.common")
+    relocate("io.grpc", "club.mcsports.droplet.party.relocate.io.grpc")
+    relocate("com.google.protobuf", "club.mcsports.droplet.party.relocate.google.protobuf")
+    relocate("com.google.common", "club.mcsports.droplet.party.relocate.google.common")
     mergeServiceFiles()
     archiveFileName.set("${rootProject.name}-${project.name}.jar")
 }
