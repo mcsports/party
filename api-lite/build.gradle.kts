1d868a4e61e44210d31c686bd5483cecff426267
David <david@cappell.net>
2025-06-10 21:38:25 +0200

    feat: publishing

--- api-lite/build.gradle.kts
+++ api-lite/build.gradle.kts
@@ -1,8 +1,20 @@
 plugins {
     `maven-publish`
 }
+
 dependencies {
-    api(project(":shared"))
+    api(project(":api"))
+}
+
+tasks.shadowJar {
+    dependencies {
+        include(project(":api"))
+        include(project(":shared"))
+        include(dependency(libs.mcsports.proto.java.get()))
+        include(dependency(libs.mcsports.proto.kotlin.get()))
+    }
+    mergeServiceFiles()
+    archiveFileName.set("${rootProject.name}-${project.name}.jar")
 }
 
 publishing {
